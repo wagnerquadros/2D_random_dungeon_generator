7ddc2a4821019c51f771b99e8a2e78516c30d075
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-08-03 11:01:14 -0300

    Player Movement FlipX

---
+++ 2D Random Dungeon Generator/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Transform GFX;
+    float flipX;
+
+    void Start()
+    {
+        GFX = GetComponentInChildren<SpriteRenderer>().transform;
+        flipX = GFX.localScale.x;
+    }
+
+
+    void Update()
+    {
+        float horz = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
+        if(Mathf.Abs(horz) > 0)
+        {
+            GFX.localScale = new Vector2(flipX * horz, GFX.localScale.y);
+        }
+    }
+}
