857de4ef39eefeef0d3bd40905accdaa67a640ea
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-08-04 12:17:10 -0300

    Dungeon Manager and Tile Spawner

---
+++ 2D Random Dungeon Generator/Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+
+    public GameObject floorPrefab, wallPrefab;
+
+    [HideInInspector] public float minX, maxX, minY, maxY;
+
+}
